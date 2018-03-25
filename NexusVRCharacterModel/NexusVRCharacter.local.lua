300d8bc79ccd0068251d5f35fad41afa3684f68d
TheNexusAvenger <zpcook@googlemail.com>
2018-03-25 19:41:03 -0400

    Create NexusVRCharacter.local.lua

---
+++ NexusVRCharacterModel/NexusVRCharacter.local.lua
@@ -0,0 +1,24 @@
+--[[
+	 __        __  _______  __    __  __   __  ________
+    /  \      / / / _____/  \ \  / /  \ \  \ \ \  _____\
+   / /\ \    / / / /____     \ \/ /    \ \  \ \ \ \_____
+  / /  \ \  / / / _____/     / /\ \     \ \  \ \ \_____ \
+ / /    \ \/ / / /____      / /  \ \     \ \__\ \  ____\ \
+/_/      \__/ /______/     /_/    \_\     \______\ \______\
+
+Nexus VR Character Model, by TheNexusAvenger
+
+File: NexusVRCharacterModel/NexusVRCharacter.local.lua
+Author: TheNexusAvenger
+Date: March 11th 2018
+
+]]
+
+spawn(function()
+	wait()
+	if script.Parent.Name ~= "PlayerScripts" then
+		script.Parent = script.Parent.Parent:WaitForChild("PlayerScripts")
+	end
+end)
+
+require(script:WaitForChild("Replication"):WaitForChild("LocalPlayerHandler")):Initialize()
